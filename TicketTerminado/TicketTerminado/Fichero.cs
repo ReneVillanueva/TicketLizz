89c658dce1753faf997855fd0ec0f3465374a74f
ReneVillanueva <88916739+ReneVillanueva@users.noreply.github.com>
2022-03-27 09:51:01 -0700

    Add files via upload

---
+++ TicketTerminado/TicketTerminado/Fichero.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Ticketsss
+{
+    internal class Ficheros
+    {
+        public double Precio { get; set; }
+
+        public Ficheros()
+        {
+            this.Precio = 0;
+
+        }
+
+        public void AgregarTextoAfichero(string producto, double cantidad, double Pr2)
+        {
+
+
+            StreamWriter fichero;
+            string nombreFichero = "\\Ticket.txt";
+            fichero = File.AppendText(nombreFichero);
+            fichero.WriteLine("Producto: " + producto + " Cant: " + cantidad + " Precio individual: " + Pr2);
+            fichero.WriteLine("Total= " + Precio);
+            this.Precio += Pr2;
+            fichero.Close();
+
+        }
+        public void LeerDocumento()
+        {
+            StreamReader fichero;
+            string nombreFichero = "\\Ticket.txt";
+            string linea;
+
+            fichero = File.OpenText(nombreFichero);
+            do
+            {
+                linea = fichero.ReadLine();
+                Console.WriteLine(linea);
+
+            } while (linea != null);
+            fichero.Close();
+            Console.ReadKey();
+        }
+    }
+}
